1cd164711740ea25e1874ce5ea183e128f38a1a8
Michael Hallquist <michael.hallquist@gmail.com>
2021-10-08 21:54:02 -0400

    draft of all post-fmriprep steps

--- R/post_fmriprep.R
+++ R/post_fmriprep.R
@@ -2,6 +2,31 @@
 
 #nfsa
 
+# matrix must be time x units/regions
+mat_to_nii <- function(mat, ni_out="mat") {
+  require(oro.nifti)
+
+  # this always puts regressors along the x dimension; y and z are singletons
+  ydim <- zdim <- 1 # size of y and z dimensions
+  xsz <- ysz <- zsz <- 1 # voxel size in x y z
+  tr <- 1
+  xorigin <- yorigin <- zorigin <- 0
+
+  system(glue("fslcreatehd {nrow(mat)} {ydim} {zdim} {ncol(mat)} {xsz} {ysz} {zsz} {tr} {xorigin} {yorigin} {zorigin} 64 {ni_out}"))
+
+  ## read empty NIfTI into R
+  nif <- readNIfTI(ni_out, reorient = FALSE)
+  nif <- drop_img_dim(nif) # need to cleanup dim_ attribute to avoid writeNIfTI failure
+
+  # populate nifti
+  nif@.Data <- array(mat, dim = c(nrow(mat), 1, 1, ncol(mat))) # add singleton dimensions for y and z
+
+  # write NIfTI with regressors back to file
+  writeNIfTI(nif, filename = ni_out)
+}
+
+
+
 runFSLCommand <- function(args, fsldir=NULL, stdout=NULL, stderr=NULL) {
   #look for FSLDIR in system environment if not passed in
   if (is.null(fsldir)) {
@@ -25,49 +50,106 @@ runFSLCommand <- function(args, fsldir=NULL, stdout=NULL, stderr=NULL) {
   
   #Sys.setenv(LD_LIBRARY_PATH="/gpfs/group/mnh5174/default/sw/openblas/lib")
   Sys.setenv(FSLDIR=fsldir) #export to R environment
-  fslsetup=paste0("FSLDIR=", fsldir, "; PATH=${FSLDIR}/bin:${PATH}; . ${FSLDIR}/etc/fslconf/fsl.sh; ${FSLDIR}/bin/")
-  fslcmd=paste0(fslsetup, args)
+  fslsetup <- paste0("FSLDIR=", fsldir, "; PATH=${FSLDIR}/bin:${PATH}; . ${FSLDIR}/etc/fslconf/fsl.sh; ${FSLDIR}/bin/")
+  fslcmd <- paste0(fslsetup, args)
   if (!is.null(stdout)) { fslcmd=paste(fslcmd, ">", stdout) }
   if (!is.null(stderr)) { fslcmd=paste(fslcmd, "2>", stderr) }
-  cat("FSL command: ", fslcmd, "\n")
-  retcode <- system(fslcmd)
-  return(retcode)
+  #cat("FSL command: ", fslcmd, "\n")
+  cat(args, "\n")
+  #retcode <- system(fslcmd)
+  #return(retcode)
+  return(NULL)
 }
 
-temporal_filter <- function(in_file, prefix="f", low_hz=1/120, high_hz=Inf tr=NULL) {
-  checkmate::assert_file_exists(in_file)
-  checkmate::assert_string(prefix)
-  checkmate::assert_number(low_hz)
-  checkmate::assert_number(high_hz)
-  checkmate::assert_number(tr, lower=0.01, upper=30)
+out_file_exists <- function(in_file, prefix, overwrite=TRUE) {
+  # handle extant file
+  out_file <- glue("{prefix}{in_file}")
+  skip <- FALSE
+  if (checkmate::test_file_exists(out_file)) {
+    if (isFALSE(overwrite)) {
+      message(glue("Processed image already exists: {out_file}. Skipping this step."))
+      skip <- TRUE
+    } else {
+      message(glue("Overwriting image: {out_file}."))
+    }
+  }
+  return(list(out_file=out_file, skip=skip))
+}
 
-  if (is.infinite(high_hz)) {
-    message("No high-pass filtering")
+temporal_filter <- function(in_file, prefix="f", low_pass_hz=0, high_pass_hz=1/120, tr=NULL, overwrite=FALSE) {
+  #checkmate::assert_file_exists(in_file)
+  checkmate::assert_string(prefix)
+  checkmate::assert_number(low_pass_hz)
+  checkmate::assert_number(high_pass_hz)
+  checkmate::assert_number(tr, lower = 0.01, upper = 30)
+  stopifnot(low_pass_hz < high_pass_hz)
+
+  # handle extant file
+  res <- out_file_exists(in_file, prefix, overwrite)
+  if (isTRUE(res$skip)) {
+    return(res$out_file) # skip out
+  } else  {
+    out_file <- res$out_file
   }
 
   # bptf specifies its filter cutoffs in terms of volumes, not frequencies
   fwhm_to_sigma <- sqrt(8 * log(2)) # Details here: https://www.mail-archive.com/hcp-users@humanconnectome.org/msg01393.html
-  lp_volumes <- 1 / (low_hz * fwhm_to_sigma * tr)
-  
 
+  if (is.infinite(high_pass_hz)) {
+    #message("Low-pass filtering")
+    hp_volumes <- -1 # do not apply high-pass
+  } else {
+    hp_volumes <- 1 / (high_pass_hz * fwhm_to_sigma * tr)
+  }
+
+  if (is.infinite(low_pass_hz) || low_pass_hz==0) {
+    #message("High-pass filtering")
+    lp_volumes <- -1 # do not apply low-pass
+  } else {
+    lp_volumes <- 1 / (low_pass_hz * fwhm_to_sigma * tr)
+  }
+
+  temp_tmean <- tempfile()
+  runFSLCommand(glue("fslmaths {in_file} -Tmean {temp_tmean}"))
+  runFSLCommand(glue("fslmaths {in_file} -bptf {hp_volumes} {lp_volumes} -add {out_file} "))
+  return(out_file)
 }
 
-apply_aroma <- function(in_file, out_file, in_confounds, out_confounds) {
+apply_aroma <- function(in_file, brain_mask=NULL, prefix="a", mixing_file, noise_file, overwrite=FALSE) {
+  #checkmate::assert_file_exists(in_file)
+  checkmate::assert_string(prefix)
+  checkmate::assert_file_exists(mixing_file)
+  checkmate::assert_file_exists(noise_file)
 
+  # handle extant file
+  res <- out_file_exists(in_file, prefix, overwrite)
+  if (isTRUE(res$skip)) {
+    return(res$out_file) # skip out
+  } else {
+    out_file <- res$out_file
+  }
+
+  #just read in the comma-separated noise ICs
+  noise_ics <- readLines(noise_file, warn=FALSE)
+
+  cmd <- glue("fsl_regfilt -i {in_file} -o {out_file} -d {mixing_file} -f {noise_ics}")
+  if (!is.null(brain_mask) && checkmate::test_file_exists(brain_mask)) {
+    cmd <- glue("{cmd} -m {brain_mask}")
+  }
+
+  runFSLCommand(cmd)
+  return(out_file)
 }
 
 spatial_smooth <- function(in_file, prefix="s", fwhm_mm=6, brain_mask=NULL) {
-  checkmate::assert_file_exists(in_file)
+  #checkmate::assert_file_exists(in_file)
 
   # handle extant file
-  out_file <- glue("{prefix}{in_file}")
-  if (checkmate::test_file_exists(out_file)) {
-    if (isFALSE(overwrite)) {
-      message(glue("Spatially smoothed image already exists: {out_file}. Skipping this step.")
-      return(out_file)
-    } else {
-      message(glue("Overwriting spatially smoothed image: {out_file}.")
-    }
+  res <- out_file_exists(in_file, prefix, overwrite)
+  if (isTRUE(res$skip)) {
+    return(res$out_file) # skip out
+  } else {
+    out_file <- res$out_file
   }
 
   fwhm_to_sigma <- sqrt(8 * log(2)) # Details here: https://www.mail-archive.com/hcp-users@humanconnectome.org/msg01393.html
@@ -87,7 +169,7 @@ spatial_smooth <- function(in_file, prefix="s", fwhm_mm=6, brain_mask=NULL) {
 }
 
 get_image_quantile <- function(in_file, brain_mask=NULL, quantile=50) {
-  checkmate::assert_file_exists(in_file)
+  #checkmate::assert_file_exists(in_file)
   checkmate::assert_number(quantile, lower=0, upper=100)
   if (is.null(brain_mask)) {
     quantile_value <- runFSLCommand(glue("fslstats {in_file} -P {quantile}")) # median of non-zero voxels
@@ -99,19 +181,16 @@ get_image_quantile <- function(in_file, brain_mask=NULL, quantile=50) {
 }
 
 intensity_normalize <- function(in_file, prefix="n", brain_mask=NULL, global_median=10000, overwrite=FALSE) {
-  checkmate::assert_file_exists(in_file)
+  #checkmate::assert_file_exists(in_file)
   checkmate::assert_string(prefix)
   checkmate::assert_number(global_median)
 
   # handle extant file
-  out_file <- glue("{prefix}{in_file}")
-  if (checkmate::test_file_exists(out_file)) {
-    if (isFALSE(overwrite)) {
-      message(glue("Intensity normalized image already exists: {out_file}. Skipping this step.")
-      return(out_file)
-    } else {
-      message(glue("Overwriting intensity-normalized image: {out_file}.")
-    }
+  res <- out_file_exists(in_file, prefix, overwrite)
+  if (isTRUE(res$skip)) {
+    return(res$out_file) # skip out
+  } else {
+    out_file <- res$out_file
   }
 
   median_intensity <- get_image_quantile(in_file, brain_mask, 50)
@@ -121,14 +200,43 @@ intensity_normalize <- function(in_file, prefix="n", brain_mask=NULL, global_med
   return(out_file)
 }
 
-process_subject <- function(in_file, processing_steps) {
+get_fmriprep_outputs <- function(in_file) {
+  prefix <- sub("(sub-\\d+_task-[^_]+_run-\\d+).*", "\\1", in_file, perl=TRUE)
+  bold <- Sys.glob(glue("{prefix}*preproc_bold*nii*"))
+  mask <- Sys.glob(glue("{prefix}*_desc-brain_mask*nii*"))
+  confounds <- glue("{prefix}_desc-confounds_regressors.tsv")
+  melodic_mix <- glue("{prefix}_desc-MELODIC_mixing.tsv")
+  noise_ics <- glue("{prefix}_AROMAnoiseICs.csv")
+  ret_list <- list(bold = bold, mask = mask, confounds = confounds, melodic_mix = melodic_mix, noise_ics = noise_ics)
+  ret_list <- lapply(ret_list, function(x) {
+    if (!checkmate::test_file_exists(x)) {
+      return(NULL)
+    } else {
+      return(x)
+    }
+  })
+  return(ret_list)
+}
+
+process_subject <- function(in_file, processing_steps=list()) {
   checkmate::assert_file_exists(in_file)
   checkmate::assert_list(processing_steps)
+  proc_files <- get_fmriprep_outputs(in_file)
 
-  for (step in processing_steps) {
-    if (step$name == "spatial_smooth") {
+  res <- spatial_smooth(proc_files$bold, brain_mask=proc_files$mask)
+  res <- apply_aroma(res, brain_mask=proc_files$mask, mixing_file=proc_files$melodic_mix, noise_file=proc_files$noise_ics)
+  res <- temporal_filter(res, tr=1)
+  res <- intensity_normalize(res, brain_mask=proc_files$mask)
 
-    } else if (step)
+  # for (step in processing_steps) {
+  #   if (step$name == "spatial_smooth") {
 
-  }
+  #   } else if (step) {
+
+  #   }
+
+  # }
 }
+
+process_subject("sub-221256_task-clock_run-2_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz")
+
