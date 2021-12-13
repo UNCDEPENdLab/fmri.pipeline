989730371b70f8a2d4ea55dfac04bda9f45d2abe
Michael Hallquist <michael.hallquist@gmail.com>
2021-12-13 13:07:01 -0500

    Remove errant browser call

--- R/setup_l1_models.R
+++ R/setup_l1_models.R
@@ -58,8 +58,6 @@ setup_l1_models <- function(gpa, l1_model_names=NULL) {
     foreach::registerDoSEQ() #formally register a sequential 'pool' so that dopar is okay
   }
 
-  browser()
-
   # loop over each subject, identify relevant fMRI data, and setup level 1 analysis files
   all_subj_l1_list <- foreach(subj_df = iter(gpa$subject_data, by="row"), .inorder=FALSE, .packages=c("dplyr"),
     .export=c("truncate_runs", "fsl_l1_model", "get_mr_abspath",
