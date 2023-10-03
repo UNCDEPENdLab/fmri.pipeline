e34b23c5215232524bcd00bcfe116b4267d7e870
Will Asciutto <wasciutto@unc.edu>
2023-10-03 16:01:28 -0400

    Rearrange & describe existing GPA tests.

--- tests/testthat/test-gpa.R
+++ tests/testthat/test-gpa.R
@@ -1,20 +1,23 @@
 library(testthat)
 
-test_that("Basic GPA setup works", {
-  gpa <- get_gpa_minimal()
-  expect_equal(1, 1)
-})
-
-test_that("Summarize GPA setup", {
-  gpa <- get_gpa_minimal()
-  summarize_l1_models(gpa)
-  expect_equal(1, 1)
-})
-
+#' Test summarization of GPA
 test_that("Summarize GPA", {
   gpa <- get_gpa()
   summarize_pipeline(gpa)
   expect_equal(1, 1)
 })
 
-# show cwd
+#' Test edge case where the GPA is created with minimal settings
+#' Assures None values are handled correctly
+test_that("Summarize GPA minimal", {
+  gpa <- get_gpa_minimal()
+  summarize_pipeline(gpa)
+  expect_equal(1, 1)
+})
+
+#' Test summarization of L1 models
+test_that("Summarize L1 models", {
+  gpa <- get_gpa()
+  summarize_l1_models(gpa)
+  expect_equal(1, 1)
+})
