511d9598b7ebb3c2cc62250cf57b85df668fbb7f
Will Asciutto <wasciutto@unc.edu>
2023-10-03 16:01:29 -0400

    Change root datafolder of tests, as they run in context of testthat folder when using the library's commands.

--- tests/testthat/test-gpa.R
+++ tests/testthat/test-gpa.R
@@ -2,7 +2,7 @@ library(testthat)
 
 #' Test summarization of GPA
 test_that("Summarize GPA", {
-  gpa <- get_gpa()
+  gpa <- get_gpa("testdata")
   summarize_pipeline(gpa)
   expect_equal(1, 1)
 })
@@ -10,14 +10,14 @@ test_that("Summarize GPA", {
 #' Test edge case where the GPA is created with minimal settings
 #' Assures None values are handled correctly
 test_that("Summarize GPA minimal", {
-  gpa <- get_gpa_minimal()
+  gpa <- get_gpa_minimal("testdata")
   summarize_pipeline(gpa)
   expect_equal(1, 1)
 })
 
 #' Test summarization of L1 models
 test_that("Summarize L1 models", {
-  gpa <- get_gpa()
+  gpa <- get_gpa("testdata")
   summarize_l1_models(gpa)
   expect_equal(1, 1)
 })
