b0cd604dc26750b5f0e71da9d3755b325f5c2d6d
Will Asciutto <wasciutto@unc.edu>
2023-10-17 01:18:50 -0400

    Add filler job status query command.

---
+++ R/get_job_status.R
@@ -0,0 +1,8 @@
+#' Function which queries for the status of jobs associated with the given gpa object
+get_job_status <- function(gpa = NULL) {
+    # Get the path to the sqlite database from the gpa object
+
+    # Use read database function to get the status of the jobs
+
+    # Print the status of the jobs
+}
