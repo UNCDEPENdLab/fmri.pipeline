3777ab84e8bd2415edacd7f63133d2610b1e099a
Will Asciutto <wasciutto@unc.edu>
2023-11-07 13:15:17 -0500

    Move basic job to its own creation function.

--- tests/testthat/test_batch.R
+++ tests/testthat/test_batch.R
@@ -1,25 +1,41 @@
+get_simple_batch_job <- function() {
+	return(
+		R_batch_job$new(
+			batch_directory=tempdir(),
+			job_name="step1",
+			n_nodes=1,
+			n_cpus=1,
+			wall_time="10:00",
+			r_code=c(
+					"Sys.sleep(100)",
+					"print('hi')",
+					"x <- 2+2"
+			),
+			r_packages=c("lme4"),
+			batch_code = c(
+					"module use /proj/mnhallqlab/sw/modules",
+					"module load r/4.0.3_depend"
+			),
+			scheduler="slurm",
+			repolling_interval=1 #repoll every second for this test (should be slower for real jobs)
+		)
+	)
+}
+
 test_that("Basic job setup works", {
 	# note that HPC settings like nodes and batch_code are ignored for local scheduler
-	w <- R_batch_job$new(
-		batch_directory=tempdir(),
-		job_name="step1",
-		n_nodes=1,
-		n_cpus=1,
-		wall_time="10:00",
-		r_code=c(
-				"Sys.sleep(100)",
-				"print('hi')",
-				"x <- 2+2"
-		),
-		r_packages=c("lme4"),
-		batch_code = c(
-				"module use /proj/mnhallqlab/sw/modules",
-				"module load r/4.0.3_depend"
-		),
-		scheduler="slurm",
-		repolling_interval=1 #repoll every second for this test (should be slower for real jobs)
-	)
+	w <- get_simple_batch_job()
 
 	w$generate()
+	print(list.files(w$batch_directory))
+	expect_equal(1, 1)
+})
+
+test_that("Basic job submission works", {
+	# note that HPC settings like nodes and batch_code are ignored for local scheduler
+	w <- get_simple_batch_job()
+
+	w$submit()
+	print(list.files(w$batch_directory))
 	expect_equal(1, 1)
 })
