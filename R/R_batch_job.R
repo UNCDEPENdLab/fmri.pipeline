9c0f44fbd98b545ec7f16e5868ba6dbeb63a58bf
Will Asciutto <wasciutto@unc.edu>
2023-11-13 16:44:41 -0500

    Remove the batch_id insertion from batch_data, as insert_df_sqlite takes id as a parameter instead. Manually start up a DB connection for now.

--- R/R_batch_job.R
+++ R/R_batch_job.R
@@ -509,22 +509,34 @@ R_batch_job <- R6::R6Class("batch_job",
       # SUBMITTED is a special state used before state is fetched from batch scheduler to indicate
       # that the job was submitted but has a non-confirmed status
       batch_data = data.frame(
-        self$batch_id, private$job_id, self$job_name, "SUBMITTED", current_time
+        job_id = private$job_id[[1]],
+        job_name = self$job_name,
+        state = "SUBMITTED",
+        timestamp = current_time
       )
 
       # Give the above dataframe column names
-      colnames(batch_data) <- c("batch_id", "job_id", "job_name", "state", "timestamp")
+      colnames(batch_data) <- c("job_id", "job_name", "state", "timestamp")
 
       # Create a gpa list filler object that just has output_locations$sqlite_db
       # populated in order to use insert_df_sqlite
-      gpa <- list(output_locations = list(sqlite_db = self$sqlite_db))
+      gpa <- list(output_locations = list(sqlite_db = self$sqlite_db), id="123")
+      class(gpa) <- c("list", "glm_pipeline_arguments")
+
+      # Temporarily copied over from finalize_pipeline_configuration
+      if (is.null(gpa$sqlite_con) || !DBI::dbIsValid(gpa$sqlite_con)) {
+        print(paste0("Opening SQLite connection to: ", gpa$output_locations$sqlite_db))
+        gpa$sqlite_con <- DBI::dbConnect(RSQLite::SQLite(), gpa$output_locations$sqlite_db)
+      }
 
       # Insert batch_id and values into batch table here
       insert_df_sqlite(
         gpa = gpa,
         table = "job_status",
+        id = self$batch_id,
         data = batch_data,
-        append = TRUE
+        append = TRUE,
+        immediate = TRUE
       )
 
       if (!is.null(cd) && dir.exists(cd)) setwd(cd) # reset working directory (don't attempt if that directory is absent)
