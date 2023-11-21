92d84486ffc4e1ff03d073383037b30ca3a7160d
Will Asciutto <wasciutto@unc.edu>
2023-11-21 15:09:09 -0500

    Add test demonstrating dependent jobs.

--- tests/testthat/test_batch.R
+++ tests/testthat/test_batch.R
@@ -1,8 +1,8 @@
-get_simple_batch_job <- function(tmp_dir=get_temp_dir()) {
+get_simple_batch_job <- function(job_name="step1", tmp_dir=get_temp_dir()) {
 	return(
 		R_batch_job$new(
 			batch_directory=tmp_dir,
-			job_name="step1",
+			job_name=job_name,
 			n_nodes=1,
 			n_cpus=1,
 			wall_time="10:00",
@@ -23,7 +23,6 @@ get_simple_batch_job <- function(tmp_dir=get_temp_dir()) {
 }
 
 test_that("Basic job setup works", {
-	# note that HPC settings like nodes and batch_code are ignored for local scheduler
 	w <- get_simple_batch_job()
 
 	w$generate()
@@ -32,7 +31,6 @@ test_that("Basic job setup works", {
 })
 
 test_that("Basic job submission works", {
-	# note that HPC settings like nodes and batch_code are ignored for local scheduler
 	tmp_dir <- get_temp_dir()
 	
 	w <- get_simple_batch_job(tmp_dir=tmp_dir)
@@ -43,6 +41,27 @@ test_that("Basic job submission works", {
 	print(list.files(w$batch_directory))
 
 	preview_db(w$sqlite_db)
+
+	expect_equal(1, 1)
+})
+
+test_that("Nested job submission works", {
+	tmp_dir <- get_temp_dir()
+	db_path <- file.path(tmp_dir, "testdb")
+
+	parent_job <- get_simple_batch_job(job_name="parent", tmp_dir=tmp_dir)
+	parent_job$batch_id <- "test_batch_id_123"
+	parent_job$sqlite_db <- db_path
+
+	child_job <- get_simple_batch_job(job_name="child", tmp_dir=tmp_dir)
+	child_job$parent_jobs <- parent_job
+	child_job$batch_id <- "test_batch_id_124"
+	child_job$sqlite_db <- db_path
+
+	jobs <- R_batch_sequence$new(parent_job, child_job)
+	jobs$submit()
+
+	preview_db(child_job$sqlite_db)
 
 	expect_equal(1, 1)
 })
