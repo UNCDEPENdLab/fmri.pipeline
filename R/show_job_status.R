2d4add80600cbb815722ba4cbcd31461ad06ca5b
Will Asciutto <wasciutto@unc.edu>
2023-12-12 16:15:13 -0500

    Swap to not appending records - update in place.

--- R/show_job_status.R
+++ R/show_job_status.R
@@ -9,7 +9,7 @@
 #' 
 #' @return a data.frame containing the status of jobs
 #' @export
-show_job_status <- function(gpa=NULL, batch_id=NULL, days_back=1, desc=TRUE, latest_only=TRUE, update=TRUE) {
+show_job_status <- function(gpa=NULL, batch_id=NULL, days_back=1, desc=TRUE, update=TRUE) {
 
     # Use read database function to get the status of the jobs
     #status_df <- read_df_sqlite(gpa = gpa, table = "job_status", id = batch_id)
@@ -21,7 +21,7 @@ show_job_status <- function(gpa=NULL, batch_id=NULL, days_back=1, desc=TRUE, lat
         # and get the output as a list. These jobs do not have a settled state so must be
         # requeried.
         open_jobs <- status_df %>%
-            filter(state == "RUNNING" || state == "SUBMITTED") %>%
+            filter(state == "RUNNING" | state == "SUBMITTED") %>%
             pull(id)
         cat("Open jobs:\n")
         print(open_jobs)
@@ -56,51 +56,28 @@ show_job_status <- function(gpa=NULL, batch_id=NULL, days_back=1, desc=TRUE, lat
             cat("Status df open:\n")
             print(status_df_open)
 
-            # For each id in status_df_open, add record to the status table
-            for(record in status_df_open) {
-                # Add records with updated state to status table
+            # Iterate over each row in status_df_open and update the status table
+            for(i in 1:nrow(status_df_open)) {
+                # Get the row
+                row <- status_df_open[i,]
+                # Get the id
+                id <- row$id
+
+                # Update the status table
                 insert_df_sqlite(
                     gpa = gpa,
-                    id = status_df_open$id,
+                    id = id,
                     table = "job_status",
-                    data = status_df_open,
+                    data = row,
                     append = TRUE,
-                    delete_extant = FALSE
+                    overwrite = FALSE
                 )
             }
+
 
             # Re-pull the status_df with the new records
             # Could also avoid a re-pull by just updating the status_df with the new records
             status_df <- dbReadTable(conn, "job_status")
-        }
-    }
-    
-    # Narrow down status id by batch_id if it's present, otherwise use a time range
-    if(!is.null(batch_id)) {
-        # Only select records from status_df with batch ids matching the batch_id argument
-        status_df <- status_df %>%
-            filter(batch_id == batch_id)
-    } else {
-        # Select records by time range
-
-        # Get the current time
-        current_time <- Sys.time()
-
-        # Get the time range to search for jobs
-        range_seconds <- days_back*24*60*60
-        since_time <- current_time - range_seconds
-
-        # Query the status df for the latest value of every job id, within the time period of 'range' days
-        # from the current time
-        status_df <- status_df %>%
-            filter(timestamp > since_time) %>%
-        
-        if(latest_only) {
-            # Group by job and pick the latest submission time
-            status_df <- state_df %>%
-                group_by(job_id) %>%
-                filter(timestamp == max(timestamp)) %>%
-                ungroup()
         }
     }
     
@@ -119,3 +96,14 @@ show_job_status <- function(gpa=NULL, batch_id=NULL, days_back=1, desc=TRUE, lat
 
     dbDisconnect(conn)
 }
+
+# Query the status df for the latest value of every job id, within the time period of 'range' days
+# from the current time
+# status_df <- status_df %>%
+#     filter(timestamp > since_time)
+# Get the current time
+    # current_time <- Sys.time()
+
+    # # Get the time range to search for jobs
+    # range_seconds <- days_back*24*60*60
+    # since_time <- current_time - range_seconds
