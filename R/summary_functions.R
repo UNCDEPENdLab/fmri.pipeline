0f738343c19414f51e8819208c95ce8a41ccffae
Ryan Smith <ryands02@email.unc.edu>
2024-02-06 01:57:43 -0500

    add class to l1_models objects and add summary() functions (not signals)

--- R/summary_functions.R
+++ R/summary_functions.R
@@ -85,45 +85,16 @@ summary.glm_pipeline_arguments <- function(gpa) {
           cat(" -  Effective User: ", gpa$sys_info[8], "\n")
       } else {
       cat("No info in gpa object\n")
-      }
-  }
+    }
+}
 
-## Model Summaries
-## model_summary inputs a gpa object and runs an existing
-
-#model_summary <- function(gpa_object) {
-#  complete <- FALSE
-#      while (isFALSE(complete)) {
-#        model_object <- menu(c(
-#          "Events",
-#          "Signals",
-#          "Models",
-#          "All of the Above",
-#          "Exit Summary Build"), title= "Choose L1 Model Object")
-#          if (model_object == 1) {
-#            cat("\n \n")
-#            summarize_events(gpa_object$l1_models)
-#          } else if (model_object == 2) {
-#            cat("\n \n")
-#            summarize_l1_signals(gpa_object$l1_models$signals)
-#          } else if (model_object == 3) {
-#            cat("\n \n")
-#            summarize_l1_models(gpa_object$l1_models$models)
-#          } else if (model_object == 4) {
-#            cat("\n \n")
-#            summarize_events(gpa_object$l1_models)
-#            summarize_l1_signals(gpa_object$l1_models$signals)
-#            summarize_l1_models(gpa_object$l1_models$models)
-#          } else {
-#            cat("\n \n", "Goodbye.")
-#          }
-#          complete <- TRUE
-#        }
-#      }
-
-
-model_summary <- function(gpa_object) {
+#' Helper function to better summarize GPA object's L1 models
+#' @param gpa the \code{gpa} object
+#' @return a readable summary of the given gpa object's L1 models
+#' @export
+summary.l1_model_set <- function(l1_ms) {
   complete <- FALSE
+  if (!is.null(l1_ms)) {
       while (isFALSE(complete)) {
         model_object <- menu(c(
           "Events",
@@ -133,60 +104,130 @@ model_summary <- function(gpa_object) {
           "Exit Summary Build"), title= "Choose L1 Model Object")
           if (model_object == 1) {
             cat("\n \n")
-            summarize_events(gpa_object$l1_models)
+            summary(l1_ms$events)
           } else if (model_object == 2) {
-            cat("\n \n")
-            summ_l1_sig(gpa_object$l1_models$signals)
-            cat("\n")
-            y_n <- menu(c("Yes", "No"), title="More Intensive Signals Summary?")
-            if (y_n == 1) {
-              summarize_l1_signals(gpa_object$l1_models$signals)
-            } else if (y_n == 2) {
-                cat("\nGoodbye.\n\n")
-            }
+            summarize_l1_signals(l1_ms$signals)
           } else if (model_object == 3) {
             cat("\n \n")
-            summarize_l1_models(gpa_object$l1_models$models)
+            summary(l1_ms$models)
           } else if (model_object == 4) {
             cat("\n \n")
-            summarize_events(gpa_object$l1_models)
-            summarize_l1_signals(gpa_object$l1_models$signals)
-            summarize_l1_models(gpa_object$l1_models$models)
+            summary(l1_ms$events)
+            summarize_l1_signals(l1_ms$signals)
+            summary(l1_ms$models)
           } else {
             cat("\n\nGoodbye.\n")
           }
           complete <- TRUE
-        }
-      }
+    }
+  }
+}
 
-summ_l1_sig <- function(sl) {
+#' Helper function to better summarize GPA object's l1_models' events
+#' @param gpa the \code{gpa} object
+#' @return a readable summary of the given gpa object's l1_models' events
+#' @export
+summary.l1_model_set_events <- function(l1_model_set_events) {
+    cat("Summary of events available in l1 models:\n--------\n")
+    if (!is.null(l1_model_set_events)) {
+      lapply(l1_model_set_events, function(x) {
+        cat("Event name:     ", x$name, "\n")
+        cat("  Event onset:    ", x$onset, "\n")
+        cat(
+          "    mean [min -- max]: ",
+          round(mean(x$data$onset, na.rm = TRUE), 2), "[",
+          round(min(x$data$onset, na.rm = TRUE), 2), "--",
+          round(max(x$data$onset, na.rm = TRUE), 2), "]\n"
+        )
+        cat("    First 6 values:", paste(head(x$data$onset), collapse = ", "), "\n\n")
+        cat("  Event duration: ", x$duration, "\n")
+        cat(
+          "    mean [min -- max]: ",
+          round(mean(x$data$duration, na.rm = TRUE), 2), "[",
+          round(min(x$data$duration, na.rm = TRUE), 2), "--",
+          round(max(x$data$duration, na.rm = TRUE), 2), "]\n"
+        )
+        cat("    First 6 values:", paste(round(head(x$data$duration), 2), collapse = ", "), "\n\n")
+        if (!is.null(x$isi)) {
+          cat("  Event ISI/ITI:    ", x$isi, "\n")
+          cat(
+            "    mean [min -- max]: ",
+            round(mean(x$data$isi, na.rm = TRUE), 2), "[",
+            round(min(x$data$isi, na.rm = TRUE), 2), "--",
+            round(max(x$data$isi, na.rm = TRUE), 2), "]\n"
+          )
+        }
+      })
+    } else {
+      cat("No events in l1 models\n")
+    }
+}
+
+#' Helper function to better summarize GPA object's l1_models' models
+#' @param gpa the \code{gpa} object
+#' @return a readable summary of the given gpa object's l1_models' models
+#' @export
+summary.l1_model_set_models <- function(ml) {
+  if (length(ml) == 0L) { return(invisible(NULL)) }
+  cat("Summary of l1 models:\n--------\n")
+  lapply(seq_along(ml), function(ii) {
+    this <- ml[[ii]]
+    cat("--------\nModel ", ii, "\n\n")
+    cat("  Name:", this$name, "\n")
+    cat("  Signals:", paste(this$signals, collapse=", "), "\n")
+    if (ncol(this$contrasts) < 20) {
+      cat("  Contrasts:\n\n")
+      print(round(this$contrasts, 3))
+      cat("\n--------\n\n")
+    } else { cat("More than 20 regressor columns. Not printing contrasts\n") }
+  })
+}
+
+summarize_l1_signals <- function(sl) {
   if (length(sl) == 0L) {
     return(invisible(NULL))
   }
-  cat("Summary of signals available in l1 models:")
-  cat("\n--------\n")
+  cat("Summary of signals available in l1 models:\n--------\n")
   lapply(seq_along(sl), function(ii) {
     this <- sl[[ii]]
-    cat("Signal", ii, "of", length(sl), ":\n")
-    cat("  - Name:", this$name, "\n")
-    cat("  - Event alignment:", this$event, "\n")
+    cat("--------\nSignal ", ii, "\n\n")
+    cat("  Name:", this$name, "\n")
+    cat("  Event alignment:", this$event, "\n")
     if (isFALSE(this$trial_subset)) {
-      cat("  - Trial subset: FALSE", "\n")
+      cat("  Trial subset: FALSE", "\n")
     } else if (isTRUE(this$trial_subset)) {
-      cat(glue("  - Trial subset: {this$trial_subset_expression}"), "\n")
+      cat(glue("  Trial subset: {this$trial_subset_expression}"), "\n")
+      cat(
+        glue("  Proportion of trials included: overall = {round(this$trial_subset_statistics['overall'], 2)}"),
+        glue(
+          "    By id: mean = {round(this$trial_subset_statistics['mean'], 2)}, ", "sd = {round(this$trial_subset_statistics['sd'], 2)}, ",
+          "min = {round(this$trial_subset_statistics['min'], 2)}, ", "max = {round(this$trial_subset_statistics['max'], 2)}"
+        ), sep="\n"
+      )
     }
     if (this$value_type %in% c("unit", "number")) {
-      cat("  - Regressor value (constant): ", this$value_fixed[1L], "\n")
+      cat("  Regressor value (constant): ", this$value_fixed[1L], "\n")
     } else {
       cat(
-        "  - Parametric value: ", this$parametric_modulator, ", mean [min -- max]: ",
+        "  Parametric value: ", this$parametric_modulator, ", mean [min -- max]: ",
         round(mean(this$value$value, na.rm = TRUE), 2), " [ ",
         round(min(this$value$value, na.rm = TRUE), 2), " -- ",
         round(max(this$value$value, na.rm = TRUE), 2), " ] \n", sep=""
       )
     }
-    }
-  )
+    cat("  HRF Normalization:", this$normalization, "\n")
+    cat("  Add signal derivative:", as.character(this$add_deriv), "\n")
+    cat("  Demean convolved signal:", as.character(this$demean_convolved), "\n")
+    cat("  Within-subject factor:", c_string(this$wi_factors), "\n")
+    cat("  Generate beta series:", as.character(this$beta_series), "\n")
+    cat(
+      "  Multiply convolved regressor against time series:",
+      ifelse(this$ts_multiplier == FALSE || is.null(this$ts_multiplier),
+        "FALSE", this$ts_multiplier
+      ), "\n"
+    )
+    cat("\n--------\n")
+  })
 }
 
 library(yaml)
