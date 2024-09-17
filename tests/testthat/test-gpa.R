4698509162e5736c113cc8cc8894c49ef002f46b
Michael Hallquist <michael.hallquist@gmail.com>
2024-09-17 15:12:20 -0400

    Switch to s3 summary method in test

--- tests/testthat/test-gpa.R
+++ tests/testthat/test-gpa.R
@@ -3,7 +3,7 @@ library(testthat)
 #' Test summarization of GPA
 test_that("Summarize GPA", {
   gpa <- get_gpa("testdata")
-  summarize_pipeline(gpa)
+  summary(gpa)
   expect_equal(1, 1)
 })
 
@@ -11,13 +11,13 @@ test_that("Summarize GPA", {
 #' Assures None values are handled correctly
 test_that("Summarize GPA minimal", {
   gpa <- get_gpa_minimal("testdata")
-  summarize_pipeline(gpa)
+  summary(gpa)
   expect_equal(1, 1)
 })
 
 #' Test summarization of L1 models
 test_that("Summarize L1 models", {
   gpa <- get_gpa("testdata")
-  summarize_l1_models(gpa)
+  summary(gpa)
   expect_equal(1, 1)
 })
